6405a66683622c471b82fd913cc9468a721ad57e
KOUHEY0530 <mkoiruka0530@gmail.com>
2025-05-15 17:27:00 +0900

    3回目のコミット(再再々レビュー)

--- beyproject/settings.py
+++ beyproject/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -134,5 +134,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, "beylog", "static"),
 ]
